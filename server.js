2f8b9a7a418d2d0ef4fd4eb2c8babb45e8dc2ef0
Chris McCutcheon <cmccutcheon2015@gmail.com>
2016-07-29 11:23:36 +0530

    added socket emit

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
-  console.log('User connected via socket.io!');
+io.on('connection', function (socket) {
+	console.log('User connected via socket.io!');
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the chat application!'
+	});
 });
 
-http.listen(PORT, function(){
-  console.log("Server started" + PORT);
+http.listen(PORT, function () {
+	console.log('Server started!');
 });
